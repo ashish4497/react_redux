fd763fde577b2d5693aa1145abffc8c6e87fe4fb
ashish4497 <ak8393267@gmail.com>
2019-01-09 00:17:21 +0530

    maripplan logout

--- marioplan/src/Components/Store/Reducer/AuthReducer.js
+++ marioplan/src/Components/Store/Reducer/AuthReducer.js
@@ -1,6 +1,26 @@
-const initState ={}
-
-const authReducer = (state = initState, action) => {
-  return state
+const initState ={
+  authReeor : null
 }
+
+const authReducer = (state = initState, action) => {    
+  switch(action.type) { 
+    case 'LOGIN_ERROR' :
+    return {
+      ...state,
+      authError: 'Login Failed'
+    }
+    case 'LOGIN_SUCESS' :
+    console.log("loginsucess")
+      return {
+        ...state,
+        authError: null
+      }
+      case 'SIGNOUT_SUCCESS' : 
+      console.log("signout sucess")
+      return state;
+      default:
+       return state
+  }
+}
+
 export default authReducer;
